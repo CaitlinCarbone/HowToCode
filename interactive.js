67bd8074d0167284cf0f00b141aab5882db1b225
Tess234 <99089153+Tess234@users.noreply.github.com>
2023-12-12 14:08:34 -0500

    Update interactive.js

--- interactive.js
+++ interactive.js
@@ -39,34 +39,50 @@
 
 </div>
 
-   <script>
-    var correctAnswers = [
-        "def add(x, y):", 
-        "def first(l):",
-        "<div></div>", 
-        "int multiply(int x, int y) { return x * y; }",
-        'cout << "Hello World!";',
-    ];
+ var correctAnswers = [
+    "def add(x, y):", 
+    "def first(l):",
+    "<div></div>", 
+    "int multiply(int x, int y) { return x * y; }",
+    'cout << "Hello World!";',
+];
 
-    function checkAnswer(questionNumber) {
-        console.log("checkAnswer function called for question " + questionNumber);
+function checkAnswer(questionNumber) {
+    console.log("checkAnswer function called for question " + questionNumber);
 
-        var userInputId = "userAnswer" + questionNumber;
-        var userAnswer = document.getElementById(userInputId).value.trim().toLowerCase();
+    var userInputId = "userAnswer" + questionNumber;
+    var userAnswer = document.getElementById(userInputId).value.trim().toLowerCase();
 
-        var correctAnswer = correctAnswers[questionNumber - 1];
+    var correctAnswer = correctAnswers[questionNumber - 1];
 
-        console.log("User Answer: " + userAnswer);
-        console.log("Correct Answer: " + correctAnswer);
+    console.log("User Answer: " + userAnswer);
+    console.log("Correct Answer: " + correctAnswer);
 
-        var resultId = "result" + questionNumber;
-        var resultElement = document.getElementById(resultId);
+    var resultId = "result" + questionNumber;
+    var resultElement = document.getElementById(resultId);
 
-        if (userAnswer === correctAnswer) {
-            resultElement.innerHTML = "Correct!";
-        } else {
-            resultElement.innerHTML = "Incorrect. Try again!";
-        }
-        document.getElementById(userInputId).value = "";
+    var inputElement = document.getElementById(userInputId);
+
+    if (userAnswer === correctAnswer) {
+        inputElement.style.backgroundColor = "green";
+        resultElement.innerHTML = "Correct!";
+    } else {
+        inputElement.style.backgroundColor = "red";
+        resultElement.innerHTML = "Incorrect. Try again!";
+        fadeOut(inputElement);
     }
-</script>
+}
+
+function fadeOut(element) {
+    var opacity = 1;
+    var interval = setInterval(function () {
+        if (opacity <= 0.1) {
+            clearInterval(interval);
+            element.style.display = "none";
+            element.style.backgroundColor = ""; // Reset background color
+        } else {
+            element.style.opacity = opacity;
+            opacity -= 0.1;
+        }
+    }, 100);
+}
