9f3cf1672a31ad90da83dd0644e8f1d3d32a5708
Tess234 <99089153+Tess234@users.noreply.github.com>
2023-12-12 12:18:26 -0500

    Update interactive.js

--- interactive.js
+++ interactive.js
@@ -1,16 +1,72 @@
-<script>
-    function checkAnswer() {
-        // Get the user's answer
-        var userAnswer = document.getElementById("userAnswer").value.toLowerCase(); // Convert to lowercase for case-insensitive comparison
+<!-- Add an input field and button for each question -->
+<div>
+    <h3>Can you find the bug? </h3>
+    <p>Here are a couple functions each with a single syntax error preventing them from working as intended. Try to find the line that contains the error! in this <a href="./quiz.html">short quiz</a>.</p>
 
-        // Define the correct answer
-        var correctAnswer = "your_actual_answer"; // Replace with the actual answer
+    <!-- Repeat this block for each question -->
+    <div>
+        <label for="userAnswer1">Question 1: Enter your answer</label>
+        <input type="text" id="userAnswer1" placeholder="Type your answer">
+        <button onclick="checkAnswer(1)">Submit</button>
+        <p id="result1"></p>
+    </div>
+
+    <div>
+        <label for="userAnswer2">Question 2: Enter your answer</label>
+        <input type="text" id="userAnswer2" placeholder="Type your answer">
+        <button onclick="checkAnswer(2)">Submit</button>
+        <p id="result2"></p>
+    </div>
+
+    <div>
+        <label for="userAnswer3">Question 3: Enter your answer</label>
+        <input type="text" id="userAnswer3" placeholder="Type your answer">
+        <button onclick="checkAnswer(3)">Submit</button>
+        <p id="result3"></p>
+    </div>
+  
+    <div>
+        <label for="userAnswer4">Question 4: Enter your answer</label>
+        <input type="text" id="userAnswer4" placeholder="Type your answer">
+        <button onclick="checkAnswer(4)">Submit</button>
+        <p id="result4"></p>
+    </div>
+    
+    <div>
+        <label for="userAnswer5">Question 5: Enter your answer</label>
+        <input type="text" id="userAnswer5" placeholder="Type your answer">
+        <button onclick="checkAnswer(5)">Submit</button>
+        <p id="result5"></p>
+    </div>
+
+</div>
+
+<script>
+    // Define an array of correct answers
+    var correctAnswers = [
+        "def add(x, y)",
+        "def first(l):",
+        "<.div> ... <./div>",
+        "int multiply(int x, int y) { return x * y; }",
+        "cout << "Hello World!";",
+        // Add correct answers for the remaining questions
+    ];
+
+    // Define a function to check the answer for a specific question
+    function checkAnswer(questionNumber) {
+        // Get the user's answer
+        var userInputId = "userAnswer" + questionNumber;
+        var userAnswer = document.getElementById(userInputId).value.toLowerCase();
+
+        // Get the correct answer for the corresponding question
+        var correctAnswer = correctAnswers[questionNumber - 1];
 
         // Check if the user's answer is correct
+        var resultId = "result" + questionNumber;
         if (userAnswer === correctAnswer) {
-            document.getElementById("result").innerHTML = "Correct!";
+            document.getElementById(resultId).innerHTML = "Correct!";
         } else {
-            document.getElementById("result").innerHTML = "Incorrect. Try again!";
+            document.getElementById(resultId).innerHTML = "Incorrect. Try again!";
         }
     }
 </script>
