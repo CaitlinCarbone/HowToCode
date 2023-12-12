eee36872ab77ced8c7cac0d2139a3827a06ff589
Tess234 <99089153+Tess234@users.noreply.github.com>
2023-12-12 14:31:14 -0500

    Update interactive.js

--- interactive.js
+++ interactive.js
@@ -44,7 +44,7 @@
     "def first(l):",
     "<div></div>", 
     "int multiply(int x, int y) { return x * y; }",
-    "cout<<'Hello World!';",
+    "cout <<",
 ];
 
 function checkAnswer(questionNumber) {
